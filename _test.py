a134c03d8495864c4e8b04de7520edb452b29f0f
SaswatiGhosh <saswatititli99@gmail.com>
2025-06-08 11:16:57 +0530

    CI implemented

---
+++ _test.py
@@ -0,0 +1,26 @@
+import pytest
+
+def square(n):
+    return n * n
+
+def cube(n):
+    return n * n * n
+
+def fifth(n):
+    return n * n * n * n * n
+
+def test_square():
+    assert square(5) == 25, "Test failed"
+    assert square(3)==9, "Test failed"
+
+def test_cube():
+    assert cube(5) == 125, "Test failed"
+    assert cube(3) == 27, "Test failed"
+
+def test_fifth():
+    assert fifth(5) == 3125, "Test failed"
+    assert cube(3) == 243, "Test failed"
+
+def test_invalid():
+    with pytest.raises(TypeError):
+        square("string")
---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title("Power Calculator")
+st.write("This is a simple power calculator that can calculate the power of a number.")
+
+n=st.numberinput("Enter a number" , value=1, step=1)
+
+square=n**2
+cube=n**3
+fifth=n**5
+
+st.write(f"The sqaure of {n} is :",{square})
+st.write(f"The cube of {n} is :",{cube})
+st.write(f"The fifth power of {n} is :",{fifth})
