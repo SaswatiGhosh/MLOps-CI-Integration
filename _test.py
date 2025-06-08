4e4ec8f005970b9db0fa4bd0bc20a2b8ca30b21f
SaswatiGhosh <saswatititli99@gmail.com>
2025-06-08 11:20:01 +0530

    CI implemente with a small change

--- _test.py
+++ _test.py
@@ -19,7 +19,7 @@ def test_cube():
 
 def test_fifth():
     assert fifth(5) == 3125, "Test failed"
-    assert cube(3) == 243, "Test failed"
+    assert fifth(3) == 243, "Test failed"
 
 def test_invalid():
     with pytest.raises(TypeError):
